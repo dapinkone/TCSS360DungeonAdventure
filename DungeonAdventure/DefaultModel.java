069353a535f1b62f660c5a4513ab3270b6012416
dapinkone <peter.iriarte24@gmail.com>
2022-11-01 16:29:25 -0700

    commented out instantiation of Dungeon() Awaiting Hero class

--- DungeonAdventure/DefaultModel.java
+++ DungeonAdventure/DefaultModel.java
@@ -3,7 +3,8 @@ package DungeonAdventure;
 public class DefaultModel implements DungeonModel {
     private Dungeon myDungeon;
     public DefaultModel() {
-        myDungeon = new Dungeon();
+        // TODO: I need a Hero() https://www.youtube.com/watch?v=bWcASV2sey0
+        //myDungeon = new Dungeon(10, 10, new Hero());
     }
 
     public void saveGame() {
