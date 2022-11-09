ddb5b3dd2437fe3726969f436f3e1260c941a7d5
dapinkone <peter.iriarte24@gmail.com>
2022-11-08 16:34:07 -0800

    added Dungeon.toString, Edge.orientation, Room[][] population code

--- DungeonAdventure/Dungeon.java
+++ DungeonAdventure/Dungeon.java
@@ -1,21 +1,37 @@
 package DungeonAdventure;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Dungeon {
     private Room[][] myRooms;
     private int rows;
     private int columns;
 
-    private static Random RANDOM = new Random();
+    private static final Random RANDOM = new Random();
     public Dungeon(int rows, int columns, Hero theHero) {
         this.rows = rows;
         this.columns = columns;
         this.myHero = theHero;
+        this.generateMaze();
     }
 
     public Hero getMyHero() {
         return myHero;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Dungeon{");
+        sb.append("myRooms=").append(Arrays.stream(myRooms)
+                .map(row -> Arrays.toString(row))
+                .collect(Collectors.joining("\n")));
+        sb.append(", rows=").append(rows);
+        sb.append(", columns=").append(columns);
+        sb.append(", myHero=").append(myHero);
+        sb.append(", myHeroLocation=").append(myHeroLocation);
+        sb.append('}');
+        return sb.toString();
     }
 
     public void setMyHero(Hero theHero) {
@@ -23,12 +39,13 @@ public class Dungeon {
     }
 
     private Hero myHero;
-    public void setMyHeroLocation(int[] theHeroLocation) {
+    public void setMyHeroLocation(Pair theHeroLocation) {
         // TODO: data validation
         this.myHeroLocation = theHeroLocation;
     }
 
-    private int[] myHeroLocation; // TODO: potentially a Pair() or Coordinates() class here?
+    private Pair myHeroLocation;
+
     private class Edge {
 
         public final Pair coordA;
@@ -59,6 +76,23 @@ public class Dungeon {
         @Override
         public int hashCode() {
             return Objects.hash(coordA, coordB);
+        }
+
+        public Direction orientation(Pair origin) {
+            /***
+             * returns the direction or relation(north/south/east/west/none) of an edge in respect to a given pair
+             */
+            Pair other;
+            if(origin == coordA) other = coordB;
+            else if(origin == coordB) other = coordA;
+            else {
+                // TODO: ?? better method for handling this?
+                throw new IllegalArgumentException(origin + " is not part of " + this);
+            }
+            if(origin.row < other.row) return Direction.NORTH;
+            if(origin.row > other.row) return Direction.SOUTH;
+            if(origin.column < other.column) return Direction.WEST;
+            return Direction.EAST;
         }
     }
     private ArrayList<Edge> getEdges(Pair p) {
@@ -94,7 +128,7 @@ public class Dungeon {
 
         while(!wallListing.isEmpty()) {
             // pick a random wall/edge from the stack.
-            int pickIndex = RANDOM.nextInt(wallListing.size()-1);
+            int pickIndex = RANDOM.nextInt(wallListing.size());
             Edge wall = wallListing.get(pickIndex);
             wallListing.remove(pickIndex);
             boolean visitedA = visitedCoords.contains(wall.coordA);
@@ -120,10 +154,30 @@ public class Dungeon {
         // now we should have a list of edges in passageList that traverses the entire maze.
         // ensuring each cell/room is reachable.
         // next we need to generate the Rooms, doors, etc.
+        myRooms = new Room[rows][];
+        for(int row = 0; row < rows; row++) {
+            Room[] rowArr = new Room[columns];
+            for(int col=0; col < columns; col++) {
+                Pair roomLocation = new Pair(row, col);
 
+                Room newRoom = new Room(roomLocation);
+                for(Edge e : getEdges(roomLocation)){
+                    if(passageList.contains(e)) {
+                        System.out.println(e);
+                        // door is open in this direction
+                        Direction doorDirection = e.orientation(roomLocation);
+                        newRoom.setDoor(doorDirection);
+                    } //else {
+                        // door is unavailable
+                    //
+                }
+                rowArr[col] = newRoom;
+            }
+            myRooms[row] = rowArr;
+        }
     }
 
-    public int[] getMyHeroLocation() {
+    public Pair getMyHeroLocation() {
         return myHeroLocation;
     }
     public Item[] getCurrentRoomItems() {
