f6fd4679550abad7297e6194d12929c7f0d50c50
dapinkone <peter.iriarte24@gmail.com>
2022-11-08 16:23:41 -0800

    changed how Room doors work; added Room.toString

--- DungeonAdventure/Room.java
+++ DungeonAdventure/Room.java
@@ -1,15 +1,20 @@
 package DungeonAdventure;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Room {
     private Item[] myItems;
-    private final boolean[] myDoors;
-    public Room() {
-        myDoors = new boolean[4];
+    private final Map<Direction, Boolean> myDoors;
+    private final Pair myLocation;
+    public Room(Pair theLocation) {
+        myLocation = theLocation;
+        myDoors = new HashMap<>();
     }
-    public Room(boolean[] theDoors) {
-        myDoors = theDoors;
+    public Boolean getDoor(Direction d) {
+        return myDoors.getOrDefault(d, false);
     }
-
     public Item[] getMyItems() {
         return myItems;
     }
@@ -17,4 +22,17 @@ public class Room {
     public void setMyItems(Item[] myItems) {
         this.myItems = myItems;
     }
+
+    @Override
+    public String toString() {
+        return "Room{" + "myItems=" + Arrays.toString(myItems) +
+                ", myDoors=" + myDoors +
+                ", myLocation=" + myLocation +
+                '}';
+    }
+
+    public void setDoor(Direction d) {
+        myDoors.put(d, true);
+    }
+
 }
