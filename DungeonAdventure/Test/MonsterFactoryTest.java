f72a48399f6cd697ea6d5e409d9fd691a54ecb30
Brian Nguyen <briannguyen636@gnail.com>
2022-11-11 15:04:32 -0800

    Finished implementing SQLLite into MonsterFactory.java, along with finishing test class.

--- DungeonAdventure/Test/MonsterFactoryTest.java
+++ DungeonAdventure/Test/MonsterFactoryTest.java
@@ -9,4 +9,13 @@ public class MonsterFactoryTest {
     @Test public void MonsterFactoryCreateTest() {
         MonsterFactory monsterFactory = new MonsterFactory();
     }
+    @Test public void CreateMonsterTest() {
+        MonsterFactory monsterFactory = new MonsterFactory();
+        Monster test3 = monsterFactory.generateMonster("ogre");
+        System.out.println(test3.getStats() + "\n");
+        Monster test1 = monsterFactory.generateMonster("gremlin");
+        System.out.println(test1.getStats() + "\n");
+        Monster test2 = monsterFactory.generateMonster("skeleton");
+        System.out.println(test2.getStats());
+    }
 }
