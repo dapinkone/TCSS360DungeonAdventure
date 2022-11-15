5fee18b7d6d6922d4402d3144d06497d609a0074
dapinkone <peter.iriarte24@gmail.com>
2022-11-15 14:03:31 -0800

    added ability to "invert" directions, to look back to aid in dungeon generation.

--- DungeonAdventure/Direction.java
+++ DungeonAdventure/Direction.java
@@ -1,5 +1,18 @@
 package DungeonAdventure;
+import java.util.Map;
 
 public enum Direction {
-    NORTH, SOUTH, EAST, WEST
+    NORTH, SOUTH, EAST, WEST;
+    private static final Map<Direction, Direction> invertMap = Map.of(
+            NORTH, SOUTH,
+            SOUTH, NORTH,
+            EAST, WEST,
+            WEST, EAST
+    );
+    public Direction invert() {
+        /*
+         returns the inversion of a given direction.
+         */
+        return invertMap.get(this);
+    }
 }
