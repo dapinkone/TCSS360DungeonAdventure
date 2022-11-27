bb695e930c7f942797e0faf93c099b26283d065c
Peter Iriarte <peter.iriarte24@gmail.com>
2022-11-26 23:57:06 -0800

    added instantiation of model + view, and call to view start

--- DungeonAdventure/CaveRescue.java
+++ DungeonAdventure/CaveRescue.java
@@ -1,10 +1,14 @@
 package DungeonAdventure;
 // main program entrypoint
 public class CaveRescue {
-    private GameModel myGameModel;
-    private GameView myGameView;
-    public static void main() {
-
+    private static GameModel myGameModel;
+    private static GameView myGameView;
+    public static void main(String[] args) {
+        // instantiate a model
+        myGameModel = new DefaultModel();
+        // instantiate a view
+        myGameView = new CLIView(myGameModel);
+        myGameView.run();
     }
 
     private void saveGame() {
