6927f97264b6cf769d52a2de20e8a49c0d0f61c2
Peter Iriarte <peter.iriarte24@gmail.com>
2022-11-26 23:53:07 -0800

    added getDoors() and getMyLocation() so rooms can say where they are.

--- DungeonAdventure/Room.java
+++ DungeonAdventure/Room.java
@@ -1,32 +1,39 @@
 package DungeonAdventure;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Room {
-    private Item[] myItems;
+    private List<Item> myItems = new ArrayList<>();
     private final Map<Direction, Boolean> myDoors;
     private final Pair myLocation;
     public Room(Pair theLocation) {
         myLocation = theLocation;
         myDoors = new HashMap<>();
     }
+
+    public Pair getMyLocation() {
+        return myLocation;
+    }
+
     public Boolean getDoor(Direction d) {
         // returns true if a door is open/present. otherwise it's a locked door/wall.
         return myDoors.getOrDefault(d, false);
     }
-    public Item[] getMyItems() {
+    public Set<Direction> getDoors() {
+        return myDoors.keySet();
+    }
+    public List<Item> getMyItems() {
         return myItems;
     }
 
-    public void setMyItems(Item[] myItems) {
-        this.myItems = myItems;
+    public void addToMyItems(Item theItem) {
+        myItems.add(theItem);
     }
 
     @Override
     public String toString() {
-        return "Room{" + "myItems=" + Arrays.toString(myItems) +
+        return "Room{" + "myItems=" + myItems +
                 ", myDoors=" + myDoors +
                 ", myLocation=" + myLocation +
                 '}';
