ac3a0c4c036c76b76f79b63f0da00a5fbfcc9d98
Peter Iriarte <peter.iriarte24@gmail.com>
2022-11-27 22:45:00 -0800

    These files 100% checkstyle approved.

--- tcss/dungeonadventure/test/DungeonGenTest.java
+++ tcss/dungeonadventure/test/DungeonGenTest.java
@@ -1,36 +1,45 @@
 package tcss.dungeonadventure.test;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import tcss.dungeonadventure.Direction;
 import tcss.dungeonadventure.Dungeon;
 import tcss.dungeonadventure.Room;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
 
 public class DungeonGenTest {
+
+    /**
+     * testRightNumberOfRooms:
+     * tests that the Dungeon generation algorithm initializes a 2-d array
+     * of Room objects.
+     */
     @Test
     public void testRightNumberOfRooms() {
-        // verifying that the constructor for Dungeon creates the requested # of rooms.
-        final int maxWidth = 40, maxHeight = 40;
+        final int maxWidth = 40;
+        final int maxHeight = 40;
 
         for (int height = 1; height < maxHeight; height++) {
             for (int width = 1; width < maxWidth; width++) {
-                Dungeon d = new Dungeon(height, width);
+                final Dungeon d = new Dungeon(height, width);
                 Assertions.assertEquals(d.getRooms().length, height);
                 Assertions.assertEquals(d.getRooms()[0].length, width);
             }
         }
     }
 
+    /***
+     * tests that a 2x2 dungeon generates as we expect.
+     */
     @Test
     public void testOpenDoors2By2() {
         Dungeon.RANDOM.setSeed(0L); // tests need to be non-random.
-        Dungeon d = new Dungeon(2, 2);
+        final Dungeon d = new Dungeon(2, 2);
 
-        final Room[][] theRooms = d.getRooms();
-        final Room topLeft = theRooms[0][0];
-        final Room topRight = theRooms[0][1];
-        final Room botLeft = theRooms[1][0];
-        final Room botRight = theRooms[1][1];
+        final Room[][] roomsGrid = d.getRooms();
+        final Room topLeft = roomsGrid[0][0];
+        final Room topRight = roomsGrid[0][1];
+        final Room botLeft = roomsGrid[1][0];
+        final Room botRight = roomsGrid[1][1];
 
         // check that proper doors are open/closed as expected
         // all these doors should be open:
