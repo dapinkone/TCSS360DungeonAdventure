e7777d0b5f317368cc69529b40c092115cda9230
Peter Iriarte <peter.iriarte24@gmail.com>
2022-12-12 15:56:50 -0800

    modified DungeonCharacterTest for RecordQ

--- DungeonAdventure/Test/DungeonCharacterTest.java
+++ DungeonAdventure/Test/DungeonCharacterTest.java
@@ -1,11 +1,13 @@
 package DungeonAdventure.Test;
 
+import DungeonAdventure.ActionResultType;
 import DungeonAdventure.DungeonCharacter;
+import DungeonAdventure.RecordQ;
 import org.junit.jupiter.api.*;
 
 public class DungeonCharacterTest {
 
-    public class MockCharacter extends DungeonCharacter {
+    public static class MockCharacter extends DungeonCharacter {
         public MockCharacter(String theName, double theHitChance) {
             super(theName,50, 1,
                     theHitChance, 10, 20);
@@ -16,17 +18,26 @@ public class DungeonCharacterTest {
     public void attackTestSuccess() {
         MockCharacter test1 = new MockCharacter("test1", 1.0);
         MockCharacter test2 = new MockCharacter("test2", 0);
-        int damage = test1.attack(test2);
-        Assertions.assertEquals(damage, test2.getMyMaxHealth() - test2.getMyHealth());
+        test1.attack(test2);
+        final var result = RecordQ.getInstance().pop();
+        assert result != null;
+        // verify hit
+        Assertions.assertEquals(result.actionResultType(), ActionResultType.Hit);
+        Assertions.assertEquals( // damage done
+                result.amount(),
+                test2.getMyMaxHealth() - test2.getMyHealth());
+        Assertions.assertEquals(result.target(), test2); // attacked
+        Assertions.assertEquals(result.source(), test1); // attacker
     }
 
     @Test
     public void attackTestMiss() {
         MockCharacter test1 = new MockCharacter("test1", 1.0);
         MockCharacter test2 = new MockCharacter("test2", 0);
-        int damage = test2.attack(test1);
-        Assertions.assertEquals(0, damage);
+        test2.attack(test1);
+        final var result = RecordQ.getInstance().poll();
+        assert result != null;
+        Assertions.assertEquals(result.actionResultType(), ActionResultType.Miss);
+        Assertions.assertEquals(0, result.amount());
     }
-
-
 }
