f5a75a5cbe61d99cedb877c087a59c6f216f30aa
Peter Iriarte <peter.iriarte24@gmail.com>
2022-12-16 17:23:49 -0800

    checkstyle fixes.

--- DungeonAdventure/ActionResultType.java
+++ DungeonAdventure/ActionResultType.java
@@ -1,9 +1,16 @@
 package DungeonAdventure;
 
 public enum ActionResultType {
+    /***
+     * Action : Source has hit target.
+     */
     Hit,
+    /*** Source has missed target. */
     Miss,
+    /*** Source has critically hit target. */
     CriticalHit,
+    /*** Source has healed itself. */
     Heal,
+    /*** Source has dealt a crushing blow against target. */
     CrushingBlow
 }
